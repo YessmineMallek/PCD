02bd3f3b7ae6ac8df533557798f0a79f561d281d
Yessmine mallek <yessmine.mallek@zeta-box.com>
2024-04-08 17:51:40 +0100

    StopBack

--- backend/trainTruck/src/main/java/com/trainTruck/demo/Repository/UserRepository.java
+++ backend/trainTruck/src/main/java/com/trainTruck/demo/Repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.trainTruck.demo.Repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.trainTruck.demo.Model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
 	
 	Optional<User> findByPhoneNumber(String phoneNumber);
